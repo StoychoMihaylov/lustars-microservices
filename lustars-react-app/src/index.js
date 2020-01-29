98419a297381b169ed851cdd6b6cb3186d58807e
StoychoMihaylov <st.mihaylov.mihaylov@gmail.com>
2020-01-30 00:11:48 +0100

    react version and dependancies fixed and updated

--- lustars-react-app/src/index.js
+++ lustars-react-app/src/index.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
+import { ConnectedRouter } from 'connected-react-router';
 import { createBrowserHistory } from 'history';
 import configureStore from './store/configureStore';
 import App from './App';
--- lustars-react-app/src/store/configureStore.js
+++ lustars-react-app/src/store/configureStore.js
@@ -1,11 +1,12 @@
-import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
-import thunk from 'redux-thunk';
-import { routerReducer, routerMiddleware } from 'react-router-redux';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
+import thunk from 'redux-thunk'
+import { connectRouter, routerMiddleware } from 'connected-react-router'
 
 export default function configureStore (history, initialState) {
-  const reducers = {
 
-  };
+  const reducers = {
+    test: {}
+  }
 
   const middleware = [
     thunk,
@@ -13,15 +14,15 @@ export default function configureStore (history, initialState) {
   ];
 
   // In development, use the browser's Redux dev tools extension if installed
-  const enhancers = [];
-  const isDevelopment = process.env.NODE_ENV === 'development';
+  const enhancers = []
+  const isDevelopment = process.env.NODE_ENV === 'development'
   if (isDevelopment && typeof window !== 'undefined' && window.devToolsExtension) {
     enhancers.push(window.devToolsExtension());
   }
 
   const rootReducer = combineReducers({
     ...reducers,
-    routing: routerReducer
+    router: connectRouter(history)
   });
 
   return createStore(
