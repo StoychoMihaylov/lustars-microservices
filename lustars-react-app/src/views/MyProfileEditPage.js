fee695337f29c9b0c12c993ff079fdddd9ecda9f
StoychoMihaylov <st.mihaylov.mihaylov@gmail.com>
2020-08-11 00:01:59 +0200

    my profile refactoring

--- lustars-react-app/src/views/MyProfileEditPage.js
+++ lustars-react-app/src/views/MyProfileEditPage.js
@@ -5,10 +5,9 @@ import '../styles/views/MyProfileEditPage.css'
 class MyProfileEditPage extends Component {
     render() {
         return (
-            <div className="profile-edit-page-container">
-                <div className="profile-edit-page">
-                    <MyProfileDetailedInfo />
-                </div>
+            <div className="profile-edit-page">
+                <h1>My Profile</h1>
+                <MyProfileDetailedInfo />
             </div>
         )
     }
