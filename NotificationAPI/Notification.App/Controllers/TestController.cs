18af6fcfab596352892623419a214e032a197836
StoychoMihaylov <st.mihaylov.mihaylov@gmail.com>
2020-11-06 23:24:28 +0100

    swagger in NotificationsAPI

---
+++ NotificationAPI/Notification.App/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿namespace Notification.App.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("notification")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [Route("test-me")]
+        public IActionResult GetME()
+        {
+            return StatusCode(200, ";)");
+        }
+    }
+}
